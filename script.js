879a5151339cebc5ed2bf07dbdb975e1f03026c8
Алексей <tany--96@mail.ru>
2022-11-16 22:10:21 +0500

    Мобильные табы попробую переписать

--- script.js
+++ script.js
@@ -3,11 +3,16 @@
     const tabsContent = document.querySelectorAll('.tab-content > div')
 
     tabsCaption.forEach((tabCaption, index) => {
-        tabCaption.addEventListener('click', (event) => {
-            removeActiveTabs(tabsCaption)
-            removeActiveTabs(tabsContent)
-            tabsContent[index].classList.add('active') 
-            tabCaption.classList.add('active')
+        tabCaption.addEventListener('click', () => {
+            if(window.screen.width < 800) {
+                tabsContent[index].classList.toggle('active')
+                tabCaption.classList.toggle('active')
+            } else {
+                removeActiveTabs(tabsCaption)
+                removeActiveTabs(tabsContent)
+                tabsContent[index].classList.add('active') 
+                tabCaption.classList.add('active')
+            }
         })
     })
 
